a245f9003ca16e44ef4c044c536f5015c1439291
yoshi3 <kokono1220@gmail.com>
2021-04-15 08:51:45 +0900

    :pizza:

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,4 +1,31 @@
 class HelloController < ActionController::Base
+  protect_from_forgery
+
+  @@title = 'Page title'
+  @@message = 'A default message';
+  def initialize()
+  end
+  def beforeEachAction
+    @title = @@title
+    @message = params[:message] || @@message
+  end
   def index
+    self.beforeEachAction()
+    @value = ''
+    if request.post? then
+      @title = 'A post request'
+      @message = "you typed #{params[:input1]}."
+      @value = params[:input1]
+    end
+
+    if @title == '' || @message == '' then
+      redirect_to action: :other, 
+      params: { 
+        'message': 'from another page'
+      }
+    end
+  end
+  def other
+    self.beforeEachAction()
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'hello/index'
   get 'hello', to: 'hello#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'hello/index'
+  post 'hello', to: 'hello#index'
+  get 'hello/other'
+  get 'hello', to: 'hello#other'
 end
